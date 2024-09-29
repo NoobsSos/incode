796318157454a5483af37164e5d97134f9714093
just Nazar <nazar.mraka@gmail.com>
2024-09-29 10:37:46 +0300

    some more cors changes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
 
   app.use(cors(
     {
-      origin: "https://incode-befc.vercel.app/",
+      origin: "https://incode-befc.vercel.app",
       credentials: true, 
       methods: 'GET,POST,PUT,DELETE',
       allowedHeaders: 'Content-Type, Authorization',
--- frontend/src/services/base.service.ts
+++ frontend/src/services/base.service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const baseURL = "http://localhost:5019/";
-const baseURL = "https://incode-backend-5rm8qv1zc-noobssos-projects.vercel.app/";
+const baseURL = "https://incode-backend-id6uuecj3-noobssos-projects.vercel.app/";
 
 export const base = axios.create({
     baseURL,
