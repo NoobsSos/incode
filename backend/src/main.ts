b2abb91c9f2b7f3448a0eba4fa774296e599d0e4
just Nazar <nazar.mraka@gmail.com>
2024-09-29 10:31:59 +0300

    cors and baseURL changes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
 
   app.use(cors(
     {
-      origin: "http://localhost:5173",
+      origin: "https://incode-befc.vercel.app/",
       credentials: true, 
       methods: 'GET,POST,PUT,DELETE',
       allowedHeaders: 'Content-Type, Authorization',
